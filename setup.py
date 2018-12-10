7994142d04a5fb82986f477690bafb01b92ee9ec
David <david.cortes.rivera@gmail.com>
2018-12-10 23:41:47 +0200

    refactor blas finder

--- setup.py
+++ setup.py
@@ -6,22 +6,34 @@ from sys import platform
 import os
 
 ## https://stackoverflow.com/questions/52905458/link-cython-wrapped-c-functions-against-blas-from-numpy
-err_msg = "Could not locate BLAS library - you'll need to manually modify setup.py to add its path."
-try:
-    blas_path = numpy.distutils.system_info.get_info('blas')['library_dirs'][0]
-except:
-    if "library_dirs" in numpy.__config__.blas_mkl_info:
-        blas_path = numpy.__config__.blas_mkl_info["library_dirs"][0]
-    elif "library_dirs" in numpy.__config__.blas_opt_info:
-        blas_path = numpy.__config__.blas_opt_info["library_dirs"][0]
-    else:
-        raise ValueError(err_msg)
+def find_blas():
+    err_msg = "Could not locate MKL or OpenBLAS libraries - you'll need to manually modify setup.py to add BLAS path."
+    try:
+        blas_path = numpy.distutils.system_info.get_info('blas')['library_dirs'][0]
+    except:
+        if "library_dirs" in numpy.__config__.blas_mkl_info:
+            blas_path = numpy.__config__.blas_mkl_info["library_dirs"][0]
+        elif "library_dirs" in numpy.__config__.blas_opt_info:
+            blas_path = numpy.__config__.blas_opt_info["library_dirs"][0]
+        else:
+            raise ValueError(err_msg)
 
-if platform[:3] == "win":
-    if os.path.exists(os.path.join(blas_path, "mkl_rt.lib")):
-        blas_file = "mkl_rt.lib"
-    elif os.path.exists(os.path.join(blas_path, "mkl_rt.dll")):
-        blas_file = "mkl_rt.dll"
+    if platform[:3] == "win":
+        ext = ["dll", "lib"]
+        pref = ""
+    elif platform[:3] == "dar":
+        ext = ["dylib"]
+        pref = "lib"
+    else:
+        ext = ["so"]
+        pref = "lib"
+
+    mkl_file_names = [pref + "mkl_rt." + e for e in ext]
+    openblas_file_names = [pref + "blas." + e for e in ext] + [pref + "openblas." + e for e in ext]
+    for fname in mkl_file_names + openblas_file_names:
+        if os.path.exists(os.path.join(blas_path, fname)):
+            blas_file = fname
+            break
     else:
         import re
         blas_file = [f for f in os.listdir(blas_path) if bool(re.search("blas", f))]
@@ -29,32 +41,12 @@ if platform[:3] == "win":
             raise ValueError(err_msg)
         blas_file = blas_file[0]
 
-elif platform[:3] == "dar":
-    if os.path.exists(os.path.join(blas_path, "libblas.dylib")):
-        blas_file = "libblas.dylib"
-    elif os.path.exists(os.path.join(blas_path, "libmkl_rt.dylib")):
-        blas_file = "libmkl_rt.dylib"
-    else:
-        import re
-        blas_file = [f for f in os.listdir(blas_path) if bool(re.search("blas", f))]
-        if len(blas_file) == 0:
-            raise ValueError("Could not locate BLAS library.")
-        blas_file = blas_file[0]
-else:
-    if os.path.exists(os.path.join(blas_path, "libblas.so")):
-        blas_file = "libblas.so"
-    elif os.path.exists(os.path.join(blas_path, "libmkl_rt.so")):
-        blas_file = "libmkl_rt.so"
-    else:
-        import re
-        blas_file = [f for f in os.listdir(blas_path) if bool(re.search("blas", f))]
-        if len(blas_file) == 0:
-            raise ValueError(err_msg)
-        blas_file = blas_file[0]
+    return blas_path, blas_file
 
 ## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
 class build_ext_subclass( build_ext ):
     def build_extensions(self):
+        blas_path, blas_file = find_blas()
         compiler = self.compiler.compiler_type
         if compiler == 'msvc': # visual studio
             for e in self.extensions:
